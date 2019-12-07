feef234fa1ac979c7fcb8ddca7b5891fc1d667a0
smw mc <smeredithwilliams@gmail.com>
2019-12-06 21:17:08 -0800

    added

--- task/App.js
+++ task/App.js
@@ -1,15 +1,41 @@
 import React, { useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (e) => {
+    setEnteredGoal(e);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals, 
+      {key: Math.random().toString(), val: enteredGoal}
+    ]);
+  }
+
   return (
     <View style={styles.container}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+      <View style={styles.screen}>
         <TextInput placeholder="write here"
-          style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 5}}
+          style={styles.txtInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="Add"></Button>
+        <Button title="Add" onPress={addGoalHandler}></Button>
       </View>
+      <Text>{enteredGoal}</Text>
+      <FlatList 
+        data={courseGoals} 
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.val}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -17,8 +43,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 45,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  screen: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center',
+  },
+  txtInput: {
+    width: '80%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 5
+  },
+  listItem: {
+    padding: 10,
+    margin: 2,
+    backgroundColor: 'grey',
+    borderColor: 'black',
+    borderWidth: 1,
+  }
 });
