0b4406ee323f948cfdf708dc1ebaf69fa738eeb8
smw mc <smeredithwilliams@gmail.com>
2019-12-10 12:28:35 -0800

    added a modal function and the ability to turn it on and off while adding task

--- task/App.js
+++ task/App.js
@@ -1,5 +1,5 @@
-import React, { useState} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
@@ -8,12 +8,15 @@ import GoalInput from './components/GoalInput';
 export default function App() {
 
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddTask, setIsAddTask] = useState(false);
 
   const addGoalHandler = (goalTitle) => {
     setCourseGoals(currentGoals => [
       ...currentGoals, 
       {key: Math.random().toString(), val: goalTitle}
     ]);
+    //close modal
+    setIsAddTask(false);
   }
 
   const removeGoalHandler = (goalId) => {
@@ -24,10 +27,12 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Button title='Add Task' onPress={() => setIsAddTask(true)}/>
       <View style={styles.screen}>
-        <GoalInput onAddGoal={addGoalHandler} />
+        <GoalInput onAddGoal={addGoalHandler} didWeModal={isAddTask} />
       </View>
       <Text>o0</Text>
+      
       <FlatList 
         data={courseGoals} 
         renderItem={itemData => (
--- task/components/GoalInput.js
+++ task/components/GoalInput.js
@@ -9,14 +9,16 @@ const GoalInput = props => {
       }
 
     return (
-        <View style={styles.screen}>
-            <TextInput placeholder="write here"
-                style={styles.txtInput}
-                onChangeText={goalInputHandler}
-                value={props.enteredGoal}
-            />
-            <Button title="Add" onPress={() => props.onAddGoal(enteredGoal)}></Button>
-        </View>
+        <Modal visible={props.didWeModal} animationType="fade">
+            <View style={styles.screen}>
+                <TextInput placeholder="write heres"
+                    style={styles.txtInput}
+                    onChangeText={goalInputHandler}
+                    value={props.enteredGoal}
+                />
+                <Button title="Add" onPress={() => props.onAddGoal(enteredGoal)}></Button>
+            </View>
+        </Modal>
     )
 }
 
@@ -25,11 +27,13 @@ const styles = StyleSheet.create({
         width: '80%', 
         borderColor: 'black', 
         borderWidth: 1, 
-        padding: 5
+        padding: 5,
+        marginBottom: 2,
       },
       screen: {
-        flexDirection: 'row', 
-        justifyContent: 'space-between', 
+        flexDirection: 'column', 
+        flex: 1,
+        justifyContent: 'center', 
         alignItems: 'center',
       },     
 });
